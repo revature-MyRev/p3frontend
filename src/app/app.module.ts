519676bb0097fd5583a7a076647c41daf8df9874
aDurk47 <andrew.durkin@revature.net>
2021-11-16 17:02:46 -0500

    updated initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
+// Imported modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+// User created components
 import { AppComponent } from './app.component';
 import { PostfeedComponent } from './components/postfeed/postfeed.component';
+import { EditComponent } from './components/profile/edit/edit.component';
+import { ProfileComponent } from './components/profile/page/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, PostfeedComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    PostfeedComponent,
+    ProfileComponent,
+    EditComponent
+    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
