122c592e679c1fbde77ba449dd24683f1233fcc1
Tommy <tommy.grabowski@revature.net>
2021-11-16 11:29:39 -0500

    Add components folder, add post feed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PostfeedComponent } from './components/postfeed/postfeed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PostfeedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/postfeed/postfeed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postfeed',
+  templateUrl: './postfeed.component.html',
+  styleUrls: ['./postfeed.component.scss']
+})
+export class PostfeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
