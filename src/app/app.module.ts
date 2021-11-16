2e2b69a5724d41623a63ceda5ba655f75f2d69a7
jquick5 <jay.quick79@gmail.com>
2021-11-16 08:42:13 -0800

    Added reset file to normalize styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { PostfeedComponent } from './components/postfeed/postfeed.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    PostfeedComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PostfeedComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
