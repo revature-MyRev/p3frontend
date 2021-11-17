9da5184e003aca97e340f241794f105f8ed230c9
Tommy <tommy.grabowski@revature.net>
2021-11-17 16:52:06 -0500

    Work on feed and post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostfeedComponent } from './components/postfeed/postfeed.component';
+import { CreatepostComponent } from './components/createpost/createpost.component';
 
 @NgModule({
-  declarations: [AppComponent, PostfeedComponent],
+  declarations: [AppComponent, PostfeedComponent, CreatepostComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
