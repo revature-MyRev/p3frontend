7b616eb5e3f963514d5c6e5c3a6af082e00990f5
jquick5 <jay.quick79@gmail.com>
2021-11-17 13:47:23 -0800

    Added post-item and comment componants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostfeedComponent } from './components/postfeed/postfeed.component';
+import { PostItemComponent } from './components/post-item/post-item.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
-  declarations: [AppComponent, PostfeedComponent],
+  declarations: [AppComponent, PostfeedComponent, PostItemComponent, CommentsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
