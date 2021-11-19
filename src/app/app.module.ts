032e07b8e49b117dd41eb5538bce5dc6cefd5857
Pryde <54861097+simbapryd3@users.noreply.github.com>
2021-11-19 15:57:26 -0500

    some login&registration stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostfeedComponent } from './components/postfeed/postfeed.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+// import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
-  declarations: [AppComponent, PostfeedComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
