0a5f4ba55cc18944d020ae7a550ee9cc7b4fbf6b
Tommy <tommy.grabowski@revature.net>
2021-12-01 15:39:09 -0500

    Merge Like/Dislikes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-
 import { ProfileComponent } from './components/profile/profile/profile.component';
 import { EditDetailsComponent } from './components/profile/edit-details/edit-details.component';
 import { EditPhotoComponent } from './components/profile/edit-photo/edit-photo.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 const routes: Routes = [
-  
-  { path: 'profile/:userId', component: ProfileComponent},
-  { path: 'edit-profile', component: EditDetailsComponent},
-  { path: 'edit-photo', component: EditPhotoComponent},
-  { path: 'search-bar', component: SearchBarComponent},
-  { path: '', redirectTo: 'home', pathMatch:'full'}
+  { path: 'profile/:userId', component: ProfileComponent },
+  { path: 'edit-profile', component: EditDetailsComponent },
+  { path: 'edit-photo', component: EditPhotoComponent },
+  { path: 'search-bar', component: SearchBarComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
