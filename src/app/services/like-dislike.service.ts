184174fb740119ba4c8f93d5a1ef89d7384b0fa0
jquick5 <jay.quick79@gmail.com>
2021-11-30 16:54:40 -0800

    removed console logs

--- src/app/services/like-dislike.service.ts
+++ src/app/services/like-dislike.service.ts
@@ -29,25 +29,21 @@ export class LikeDislikeService {
   }
 
   addLike(like: Likes): Observable<Likes> {
-    console.log('Like added');
     const url = `${baseApiUrl}/likes`;
     return this.http.post<Likes>(url, like, httpOptions);
   }
 
   removeLike(like: Likes): Observable<Likes> {
-    console.log('Like removed');
     const url = `${baseApiUrl}/likes/${like.likeId}`;
     return this.http.delete<Likes>(url);
   }
 
   addDislike(dislike: Dislikes): Observable<Dislikes> {
-    console.log('Dislike added');
     const url = `${baseApiUrl}/dislikes`;
     return this.http.post<Likes>(url, dislike, httpOptions);
   }
 
   removeDislike(dislike: Dislikes): Observable<Dislikes> {
-    console.log('Dislike removed');
     const url = `${baseApiUrl}/dislikes/${dislike.dislikeId}`;
     return this.http.delete<Dislikes>(url);
   }
